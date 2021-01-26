d5879354ffbf85061cc0ca30f1c8de4fe3b80393
algirdas ubavicius <algirdasubavicius70@gmail.com>
2021-01-26 17:31:06 +0200

    Add project files.

---
+++ CandidatesContainer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paskaita10uzduotis1
+{
+    class CandidatesContainer
+    {
+        const int Cmax = 10000;
+        public int n { get; set; }
+        public Candidate[] CandidatesArray { get; set; }
+        public CandidatesContainer()
+        {
+            n = 0;
+            CandidatesArray = new Candidate[Cmax];
+        }
+        public void AddCandidate(Candidate c)
+        {
+            if (n + 1 < Cmax)
+                CandidatesArray[n++] = c;
+        }
+        public void Sort()
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                int maxInd = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (CandidatesArray[j] > CandidatesArray[maxInd])
+                        maxInd = j;
+                }
+                Candidate temp = CandidatesArray[maxInd];
+                CandidatesArray[maxInd] = CandidatesArray[i];
+                CandidatesArray[i] = temp;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+namespace Paskaita10uzduotis1
+{
+    class Program
+    {
+        const string CFd = "..\\..\\..\\Duomenys.txt";
+        const string CFr = "..\\..\\..\\Rezultatai1.txt";
+        static void Main(string[] args)
+        {
+            CandidatesContainer ListOfCandidates = new CandidatesContainer();
+
+            ReadData(CFd, ListOfCandidates);
+
+            if (File.Exists(CFr))
+                File.Delete(CFr);
+
+            PrintCandidatesTable(CFr, "Pradinis kandidatų sąrašas", ListOfCandidates);
+            ListOfCandidates.Sort();
+            PrintCandidatesTable(CFr, "Rikiuoti duomenys", ListOfCandidates);
+        }
+        static void ReadData(string file, CandidatesContainer Candid)
+        {
+            if (File.Exists(file))
+            {
+                using (StreamReader reader = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] parts;
+                        parts = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        string nameSurname = parts[0].Trim();
+                        int mindBattles = int.Parse(parts[1]);
+                        int answere = int.Parse(parts[2]);
+                        int time = int.Parse(parts[3]);
+
+                        Candidate newCandid = new Candidate(nameSurname, mindBattles, answere, time);
+                        Candid.AddCandidate(newCandid);
+                    }
+                }
+            }
+        }
+        static void PrintCandidatesTable(string file, string tableName, CandidatesContainer Candid)
+        {
+            using (var fr = File.AppendText(file))
+            {
+                fr.WriteLine(tableName);
+                if (Candid.n > 0)
+                {
+                    string tableHead = new string('-', 87) + '\n' +
+                         String.Format("{0,-20} {1,20} {2,20} {3,22}", "Vardas ir pavardė", "Prieš tai dalyvavęs", "Teisingi atsakymai", "Sugaištas laikas (sek.)") + '\n' +
+                         new string('-', 87);
+                    fr.WriteLine(tableHead);
+                    for (int i = 0; i < Candid.n; i++)
+                    {
+                        fr.WriteLine(Candid.CandidatesArray[i].ToString());
+                    }
+                    fr.WriteLine(new string('-', 87));
+                }
+                else
+                {
+                    fr.WriteLine("Sąrašas tuščias");
+                }
+                fr.WriteLine();
+            }
+        }
+    }
+}
